62150a7871b7c95330e3983b3ca366af36f4decd
Jan S. <dartanian99@wp.pl>
2025-11-06 13:57:23 +0100

    Added icons nad Term of use text

--- Runder/src/pages/Register.jsx
+++ Runder/src/pages/Register.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../lib/firebase';
 import { Link, useNavigate } from 'react-router-dom';
+import googleIcon from '../assets/google.png';
+import facebookIcon from '../assets/facebook.png';
+import appleIcon from '../assets/apple.png';
 
 export default function Register() {
   const nav = useNavigate();
@@ -89,6 +92,17 @@ export default function Register() {
       <p style={{ marginTop: 12 }}>
         Already have an account? <Link to="/login">Log in</Link>
       </p>
+
+      <div className="social-login">
+          <p className='terms-of-use'>By registering, you agree to our <b>Terms of Use</b> and <b>Privacy Policy</b>.</p>
+          <p className="social-login-label">sign in with</p>
+              <div className="social-buttons">
+                  <img src={googleIcon} alt="Google" />
+                  <img src={appleIcon} alt="Apple" />
+                  <img src={facebookIcon} alt="Facebook" />
+              </div>
+      </div>
+
     </div>
   );
 }
