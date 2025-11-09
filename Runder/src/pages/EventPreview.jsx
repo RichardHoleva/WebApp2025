d697fce9ba9dd53661aa8d0824bb332c441bb6ee
Quang Tho Hoang <147547139+HoanTheG@users.noreply.github.com>
2025-11-09 11:00:08 +0100

    Add PlaylistSection component and integrate it into EventPreview

--- Runder/src/pages/EventPreview.jsx
+++ Runder/src/pages/EventPreview.jsx
@@ -8,6 +8,7 @@ import BuddySection from '../components/previewComponents/BuddySection';
 import familyRunImage from '../assets/familyrun.png';
 import richardholeva from '../assets/richardholeva.png'
 import BuyTicket from '../components/previewComponents/BuyTicket';
+import PlaylistSection from '../components/previewComponents/PlaylistSection';
 import '../styles/preview.css';
 
 function EventPreview() {
@@ -28,6 +29,7 @@ function EventPreview() {
       privacy: 'Private',
       distance: '10 km',
       description: 'Family Run is a midweek reset built around movement, connection, and good energy. It’s a casual, social run where everyone’s welcome — no pressure, no competition, just a chance to get outside, move your body, and share a few conversations along the way. Whether you come with friends or show up solo, you’ll find an easy pace and an even easier atmosphere. It’s about showing up, doing something good for yourself, and ending the day feeling a little lighter than when you started.',
+      playlistUrl: 'https://open.spotify.com/playlist/6FIv12d0EedHZAp9ZJvguJ',
       organizer: {
         name: 'Richard Holeva',
         avatar: richardholeva,
@@ -44,6 +46,7 @@ function EventPreview() {
       <AboutSection description={event.description} />
       <DistanceSection distance={event.distance} />
       <BuddySection />
+      <PlaylistSection playlistUrl={event.playlistUrl} />
 
       {/* Add more sections: Playlist, Gallery */}
 
